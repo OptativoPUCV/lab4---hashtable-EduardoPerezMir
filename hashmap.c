a4c374f900c5c2adc6f1688b6d395fef6f4d4061
Replit user <>
2023-04-12 22:37:42 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -95,13 +95,12 @@ Pair * searchMap(HashMap * map,  char * key) {
 
     while (strcmp(map->buckets[indice]->key, key) != 0)
     {
-        
+        indice++;
         if (indice == map->capacity)
             indice = 0;
         
         if (map->buckets[indice] == NULL)
             return NULL;
-        indice++;
     }
     
     map->current = indice;
